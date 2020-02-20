9f5de66b7bf5267c181823d317814290bbd10ae3
499895280@qq.com <43745800+Tonyclqq@users.noreply.github.com>
2020-02-20 22:22:08 +0800

    引入提示框

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,37 +1,70 @@
 <template>
-  <div id="login">
-    为啥不出来字啊？
+  <div class="login-wrap">
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button type="primary" class="login-btn" @click.prevent="handleLogin">登录</el-button>
+    </el-form>
   </div>
 </template>
 
 <script>
-
-  export default {
-   name: 'login',
-    components: {},
-    props:{},
-    data () {
-      return {
-
-      };
-    },
-
-    created(){},
-
-
-    computed: {},
-
-    beforeMount() {},
-
-    mounted() {},
-
-    methods: {},
-
-    watch: {}
-
+export default {
+  name: "login",
+  components: {},
+  props: {},
+  data() {
+    return {
+      formdata: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    //登录请求
+    handleLogin() {
+      this.$http.post("login", this.formdata).then(res => {
+        //console.log(res)
+        //对象结构赋值
+        const {
+          //data,
+          meta: { msg, status }
+        } = res.data;
+        if (status === 200) {
+          //1.跳转到home
+          this.$router.push({ name: "home" });
+          //提示成功
+          this.$message.success(msg)
+        } else {
+          this.$message.warning(msg)
+        }
+      });
+    }
   }
-
+};
 </script>
 <style  scoped>
-
+.login-wrap {
+  height: 100%;
+  background-color: #324152;
+  /*弹性盒子布局 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-wrap .login-form {
+  width: 400px;
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 30px;
+}
+.login-wrap .login-btn {
+  width: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
-
+import App from './App.vue'
+//引入路由文件
+import router from './router/idnex'
+//引入全局css
+import 'assets/css/reset.css'
+//使用element-ui
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import App from './App.vue'
-import router from './router/idnex'
-//使用element-ui
 Vue.use(ElementUI);
+//引入网络请求    把aixos做成插件进行全局使用 
+import MyHttpServe from 'plugins/http'
+Vue.use(MyHttpServe)
+
 Vue.config.productionTip = false
 
 new Vue({
